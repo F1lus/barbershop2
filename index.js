42025ff8d105c315471aad1ef27d45aca133eda5
F1lus <filimon.mark@gmail.com>
2022-07-07 00:28:00 +0200

    Javítások

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
@@ -5,17 +7,17 @@ const http = require('http')
 const https = require('https')
 const fs = require('fs')
 
-const calendar = require('./model/Calendar')
-
 const appointments = require('./route/appointments')
 const reserve = require('./route/reserve')
 
 const app = express()
 
-/*const server = https.createServer({
-    key: fs.readFileSync('c0d4b_12d6b_2b2bf5b4f1f881468dd2c4ff58e1ef58.key', 'utf8'),
-    cert: fs.readFileSync('_wildcard__kenisbarbershop_hu_c0d4b_12d6b_1632348761_d183f1424bdb56fcb46650ed991ec4d7.crt', 'utf8')
-}, app)*/
+if(process.env.ENVIRONMENT === 'prod'){
+    var server = https.createServer({
+        key: fs.readFileSync('c0d4b_12d6b_2b2bf5b4f1f881468dd2c4ff58e1ef58.key', 'utf8'),
+        cert: fs.readFileSync('_wildcard__kenisbarbershop_hu_c0d4b_12d6b_1632348761_d183f1424bdb56fcb46650ed991ec4d7.crt', 'utf8')
+    }, app)
+}
 
 const server2 = http.createServer(app)
 const path = __dirname + '/build'
@@ -40,10 +42,12 @@ app.get('*', (req, res) => {
     res.sendFile(path + '/index.html')
 })
 
-/*server.listen(4000, () => {
-    console.log('Server Online!')
-})*/
+if(process.env.ENVIRONMENT === 'prod'){
+    server.listen(4000, () => {
+        console.log('HTTPS Online!')
+    })
+}
 
 server2.listen(8080, () => {
-    console.log('Server2 Online')
+    console.log('HTTP Online')
 })
